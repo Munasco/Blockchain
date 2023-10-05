7194c02acae64c054c1843d9e8a1b1ea44825ee1
Munachi Ernest-Eze <ernest-eze1869@loyolajesuit.org>
2023-10-04 23:19:50 -0400

    First Frontend Blockchain Project

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Greeter from "./Greeter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Greeter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Greeter() {
+  return (
+    <div>
+      <h2>Hello, React!</h2>
+    </div>
+  );
+}
+export default Greeter;
